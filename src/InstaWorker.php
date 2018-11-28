7b139ced910da3a9428aed8ad97e15014550e584
Andrew Iskarev <andgalatinov@gmail.com>
2018-11-28 12:16:40 +0300

    Fixes with message format

--- src/InstaWorker.php
+++ src/InstaWorker.php
@@ -8,6 +8,7 @@ use InstagramAPI\Exception\ChallengeRequiredException;
 class InstaWorker
 {
     private $instagram;
+    const BASE_URL = "https://instagram.com/p/";
 
     public function __construct()
     {
@@ -44,10 +45,11 @@ class InstaWorker
                 $push->getActionParam('id')
             )->getThread();
 
-            $username = $receivedMessage->getUsers()[0]->getUsername();
             $attachment = $this->getAttachment($receivedMessage);
+            $username = $this->instagram->username;
+            $who = $receivedMessage->getUsers()[0]->getUsername();
 
-            exec("python notifier.py $username $attachment");
+            exec("python notifier.py $username $who $attachment[0] $attachment[1]");
         });
         $push->start();
 
@@ -62,23 +64,27 @@ class InstaWorker
         $attachment = '';
 
         if($text = $receivedMessage->getItems()[0]->getText())
-            $attachment = $text;
+            $attachment = ["Message", $text];
         else if($receivedMessage->getItems()[0]->isLike())
-            $attachment = "Action:Like";
+            $attachment = ["Message", "❤"];
         else if($url = $this->toJson($receivedMessage->getItems()[0])->visual_media->media->image_versions2->candidates[0]->url)
-            $attachment = $url;
-        else if($videoShare = $this->toJson($receivedMessage->getItems()[0])->media_share->video_versions[0]->url)
-            $attachment = $videoShare;
-        else if($imageShare = $this->toJson($receivedMessage->getItems()[0]->getMediaShare())->image_versions2->candidates[0]->url)
-            $attachment = $imageShare;
+            $attachment = ["Message", $url];
+        // video: $this->toJson($receivedMessage->getItems()[0])->media_share->video_versions[0]->url
+        // image: $this->toJson($receivedMessage->getItems()[0]->getMediaShare())->image_versions2->candidates[0]->url
+        else if($share = $this->toJson($receivedMessage->getItems()[0])->media_share->code)
+            $attachment = ["Share", static::BASE_URL . $share];
         else if($animatedMedia = $this->toJson($receivedMessage->getItems()[0])->animated_media->images->fixed_height->url)
-            $attachment = $animatedMedia;
-        else if($broadcast = $this->toJson($receivedMessage->getItems()[0]->getLiveViewerInvite())->broadcast->cover_frame_url)
-            $attachment = $broadcast;
-        else if($videoStories = $this->toJson($receivedMessage->getItems()[0]->getStoryShare()->getMedia())->video_versions[0]->url)
-            $attachment = $videoStories;
-        else if($imageStories = $this->toJson($receivedMessage->getItems()[0]->getStoryShare()->getMedia())->image_versions2->candidates[0]->url)
-            $attachment = $imageStories;
+            $attachment = ["Giphy", $animatedMedia];
+        else if($broadcast = $this->toJson($receivedMessage->getItems()[0]->getLiveViewerInvite())->broadcast) {
+            $broadcastOwner = $broadcast->broadcast_owner->username;
+            $image = $broadcast->cover_frame_url;
+
+            $attachment = ["Stream-$broadcastOwner", $image];
+        }
+        // video: $this->toJson($receivedMessage->getItems()[0]->getStoryShare()->getMedia())->video_versions[0]->url
+        // image: $this->toJson($receivedMessage->getItems()[0]->getStoryShare()->getMedia())->image_versions2->candidates[0]->url
+        else if($stories = $this->toJson($receivedMessage->getItems()[0]->getStoryShare()->getMedia())->code)
+            $attachment = ["Stories", static::BASE_URL . $stories];
 
         return $attachment;
     }
--- src/notifier.py
+++ src/notifier.py
@@ -7,10 +7,9 @@ try:
     session.auth()
     api = session.get_api()
 
-    username = "Username: " + sys.argv[1]
-    api.messages.send(user_id=140261421, message= username + "\n" + sys.argv[2])
-    print('success')
+    username = "Username: " +  sys.argv[1]
+    who = "Who: " + sys.argv[2]
+    message = username + "\n" + who + "\n" + sys.argv[3] + ": " + sys.argv[4]
+    api.messages.send(user_id=140261421, message=message)
 except BaseException as e:
     print(e)
-
-print('success')
